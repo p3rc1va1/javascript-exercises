2b477f3ae20e1116307e90b233f3ec07078cd687
p3rc1va1 <35celik.baha@gmail.com>
2024-06-23 17:05:50 +0300

    Excercises from 1-7 are done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let c = (f-32) *(5/9);
+  //rounding a float
+  return Number(c.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let f = c*(9/5)+32;
+  return Number(f.toFixed(1));
 };
 
 // Do not edit below this line
